bed2da0157e5290b7d30f9202d60a4abe7bfd49c
rachiratou Esteve <rachiraesteve@rachiratous-MacBook-Pro.local>
2024-03-14 18:24:22 +0000

    added on click to images and saved pic id to local storage

--- src/components/PexelPictureCard/index.jsx
+++ src/components/PexelPictureCard/index.jsx
@@ -1,10 +1,16 @@
 import Card from "react-bootstrap/Card"
 const PexelPictureCard=({picture})=>{
-    // console.log(picture.urls.thumb)
+const handleImageSelect=(event)=>{
+const pexelID = event.target.getAttribute("data-pid");
+console.log(pexelID)
+localStorage.setItem("currentImage", pexelID)
+}
+
+
     return(
         <Card key= {picture.id} style={{width:"16em" ,padding:"12px 12px 24px 12px", margin:"10px 0",boxShadow: "1px 1px 5px grey"
 }} >
-            <Card.Img src={picture.src.small} />
+            <Card.Img src={picture.src.small} onClick={handleImageSelect} data-pid={picture.id} />
         </Card>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,55 +2,50 @@ import keys from "../../keys/unsplash.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
-import UnsplashPictureGrid from "../../components/UnsplashPictureGrid/index.jsx";
+// import UnsplashPictureGrid from "../../components/UnsplashPictureGrid/index.jsx";
 import PexelPictureGrid from "../../components/PexelPictureGrid/index.jsx";
 import Button from "react-bootstrap/Button";
 import pexelsApi from "../../utils/pexelsAPI.js";
 
 const Home = () => {
-  const [unsplashPictureArray, setUnsplashPictureArray] = useState([{}]);
+  // const [unsplashPictureArray, setUnsplashPictureArray] = useState([{}]);
   const [pexelPictureArray, setPexelPictureArray] = useState([{}]);
   const [searchTerm, setSearchTerm] = useState("");
   const [page, setPage] = useState(1);
   const [active, setActive] = useState(1);
 
   const handleInputChange = (event) => {
-    // console.log(event.target.value);
     setSearchTerm(event.target.value);
   };
   const handlePageClick = (event) => {
-    //at this point the value of page is the currently loaded page number
     const targetPage = parseInt(event.target.innerText);
     console.log(targetPage);
     handlePageChange(targetPage);
-    // setPage(targetPage);
-    // setActive(targetPage);
-    // pexelSearch(page);
+    setActive(targetPage);
   };
-  async function unsplashHandleSubmit(e) {
-    e.preventDefault();
-    console.log(searchTerm);
-    if (!searchTerm) {
-      return false;
-    }
-    try {
-      const response = await fetch(
-        "https://api.unsplash.com/search/photos?page=1&query=" +
-          searchTerm +
-          "&orientation=portrait&client_id=" +
-          keys.accessKey
-      );
-      const data = await response.json();
-      setUnsplashPictureArray(data.results);
-    } catch (error) {
-      console.log(error);
-    }
+  // async function unsplashHandleSubmit(e) {
+  //   e.preventDefault();
+  //   console.log(searchTerm);
+  //   if (!searchTerm) {
+  //     return false;
+  //   }
+  //   try {
+  //     const response = await fetch(
+  //       "https://api.unsplash.com/search/photos?page=1&query=" +
+  //         searchTerm +
+  //         "&orientation=portrait&client_id=" +
+  //         keys.accessKey
+  //     );
+  //     const data = await response.json();
+  //     setUnsplashPictureArray(data.results);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
 
-    // setPictureArray([{}])
-  }
+  //   // setPictureArray([{}])
+  // }
 
   async function pexelSearch(page) {
-    console.log(page);
     if (!searchTerm) {
       return false;
     }
@@ -72,35 +67,37 @@ const Home = () => {
   }
   const handlePageChange = (page) => {
     setPage(page);
-    pexelSearch(page)
+    pexelSearch(page);
   };
   return (
     <main className="row">
       <div className="col-4">
         <h3>Search for an Image</h3>
-        <form onSubmit={unsplashHandleSubmit}>
-          <input
-            type="text"
-            id="Search-term"
-            onChange={handleInputChange}
-            value={searchTerm}
-          />
-          <button type="submit" id="Search-btn" className="btn">
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-        </form>
-        <Button type="button" onClick={()=>pexelSearch(page)}>
-          Search with Pexel
-        </Button>
+
+        <input
+          type="text"
+          id="Search-term"
+          onChange={handleInputChange}
+          value={searchTerm}
+        />
+        <button
+          type="click"
+          onClick={() => pexelSearch(page)}
+          id="Search-btn"
+          className="btn"
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
       </div>
+      
       <div className="col-8">
         <h3>Select your Image</h3>
 
-        {unsplashPictureArray.length === 10 ? (
+        {/* {unsplashPictureArray.length === 10 ? (
           <UnsplashPictureGrid unsplashPictureArray={unsplashPictureArray} />
         ) : (
           <h3>error</h3>
-        )}
+        )} */}
       </div>
       <div className="row">
         {pexelPictureArray.length === 10 ? (
