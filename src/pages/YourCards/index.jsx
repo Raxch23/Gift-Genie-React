86c06e566fdcd1b96cd87e9f13f07f9bc780e53b
rachiratou Esteve <rachiraesteve@rachiratous-MacBook-Pro.local>
2024-03-27 16:35:21 +0000

    displayed large image and added delete function

--- src/pages/YourCards/index.jsx
+++ src/pages/YourCards/index.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Main from "../../components/Main/index.jsx";
 import "./style.css";
-import { Card, Button, Carousel } from "react-bootstrap";
+import { Card, Button, Carousel, Row, Col } from "react-bootstrap";
 
 const YourCards = () => {
-  const cardArray = JSON.parse(localStorage.getItem("saveCardArray"));
+  // const cardArray = JSON.parse(localStorage.getItem("saveCardArray"));
+const [cardArray, setCardArray]=useState(JSON.parse(localStorage.getItem("saveCardArray")))
 
   console.log(cardArray);
   const [index, setIndex] = useState(0);
@@ -18,58 +19,98 @@ const YourCards = () => {
     setIndex(selectedIndex);
   };
 
+  const handleImgDelete=(event)=>{
+    console.log(event.target.value)
+    const updatedCardArray=cardArray.filter(card =>card.id !==event.target.value)
+    setCardArray(updatedCardArray)
+  }
+
+
   return (
-    <main className="row" style={{ border: "1px solid black" }}>
-      <div className="col-4">
-        <h3>left side</h3>
-      </div>
-      <div className="col-8">
-        <h3>right side</h3>
-        {cardArray.length > 0 ? (
-          <Carousel activeIndex={index} onSelect={handleSelect}>
-            {cardArray.map((card) => (
-              <Carousel.Item key={card.id}>
-                {/* <img src={card.imgSRC}/> */}
-                <Card id="parent" className="card-container">
-                  <Card.Body>
-                    <div className="picture-div" style={{fontFamily:card.font, fontSize:card.fontSize+"pt", color:card.fontColor}}>
-                      <div id="text-box" style={{left:card.position.x+"%", top:card.position.y+"%"}}>
-                        <h5
-                          className="card-text"
-                          // style={styles.h5}
-                          id="to-name"
+    <>
+      <Row >
+        <Col lg={12}>
+          <h2>Your Cards</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={2}></Col>
+
+        <Col lg={8}>
+          {cardArray.length > 0 ? (
+            <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              indicators={false}
+              style={{ width: "50em", margin: "10px auto" }}
+            >
+              {cardArray.map((card) => (
+                <Carousel.Item key={card.id}>
+                  {/* <img src={card.imgSRC}/> */}
+                  <Card id="parent" className="card-container">
+                    <Card.Body>
+                      <div
+                        className="picture-div"
+                        style={{
+                          fontFamily: card.font,
+                          fontSize: card.fontSize + "pt",
+                          color: card.fontColor,
+                        }}
+                      >
+                        <div
+                          id="text-box"
+                          style={{
+                            left: card.position.x + "%",
+                            top: card.position.y + "%",
+                          }}
                         >
-                          {" "}
-                          {card.recipient_name}{" "}
-                        </h5>
-                        <h5
-                          className="card-text"
-                          // style={styles.h5}
-                          id="card-message"
-                        >
-                          {card.message}
-                        </h5>
-                        <h5
-                          className="card-text"
-                          // style={styles.h5}
-                          id="from-name"
-                        >
-                          {" "}
-                          {card.sender_name}{" "}
-                        </h5>
+                          <h5
+                            className="card-text"
+                            // style={styles.h5}
+                            id="to-name"
+                          >
+                            {" "}
+                            {card.recipient_name}{" "}
+                          </h5>
+                          <h5
+                            className="card-text"
+                            // style={styles.h5}
+                            id="card-message"
+                          >
+                            {card.message}
+                          </h5>
+                          <h5
+                            className="card-text"
+                            // style={styles.h5}
+                            id="from-name"
+                          >
+                            {" "}
+                            {card.sender_name}{" "}
+                          </h5>
+                        </div>
+                        <Card.Img src={card.imgSRC} className="card-img" />{" "}
                       </div>
-                      <Card.Img src={card.imgSRC} className="card-img" />{" "}
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        ) : (
-          <h1>No images found</h1>
-        )}
-      </div>
-    </main>
+                    </Card.Body>
+                  </Card>
+                  <Button type="button" className="m-1" onClick={handleImgDelete} value={card.id}  >
+                    Delete
+                  </Button>
+                  <Button type="button" className="m-1">
+                    Print
+                  </Button>
+                  <Button type="button" className="m-1">
+                    Send
+                  </Button>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          ) : (
+            <h1>No images found</h1>
+          )}
+        </Col>
+        <Col lg={2}></Col>
+      </Row>
+    </>
   );
 };
 export default YourCards;
