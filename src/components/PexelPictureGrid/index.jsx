30dd5c482d043d77d53d3a011e9f532b397afc8e
rachiratou Esteve <rachiraesteve@rachiratous-MacBook-Pro.local>
2024-03-29 23:22:42 +0000

    made home page mobile responsive

--- src/components/PexelPictureGrid/index.jsx
+++ src/components/PexelPictureGrid/index.jsx
@@ -1,8 +1,10 @@
 import PexelPictureCard from "../PexelPictureCard";
 import Pagination from "react-bootstrap/Pagination";
-import{useState} from "react";
+import { Col } from "react-bootstrap";
+
+
+
 const PexelPictureGrid = ({ pexelPictureArray,page,handlePageClick, active}) => {
-  // console.log(active)
   let items = [];
   for (let number = 1; number <= 8; number++) {
     items.push(
@@ -13,10 +15,16 @@ const PexelPictureGrid = ({ pexelPictureArray,page,handlePageClick, active}) => 
   }
 // console.log(page)
   return (
-    <div
-      className="col-12"
+    <Col
+    sm={12}
+    md={9}
+    lg={7}
+    xl={7}
+    xxl={12}
+
       style={{
         display: "flex",
+        margin:"0 auto",
         flexWrap: "wrap",
         justifyContent: "space-around",
       }}
@@ -28,7 +36,7 @@ const PexelPictureGrid = ({ pexelPictureArray,page,handlePageClick, active}) => 
         <Pagination onClick={handlePageClick}>{items}</Pagination>
         <br />
       </div>
-    </div>
+    </Col>
   );
 };
 export default PexelPictureGrid;
